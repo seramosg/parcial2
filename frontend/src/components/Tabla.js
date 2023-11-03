6e76a0bcac5e95405580e7b700cb06ee204b8896
seramosg <seramosg@unal.edu.co>
2023-11-03 16:02:14 -0500

    Se termino

--- frontend/src/components/Tabla.js
+++ frontend/src/components/Tabla.js
@@ -1,15 +1,48 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Tabla() {
+import { DataTable } from 'react-native-paper';
 
-  
+const Tabla = () => {
   return (
-    <View>
-      <Text>tabla</Text>
-      
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({})
+    <DataTable style={styles.container}>
+      <DataTable.Header style={styles.tableHeader}>
+        <DataTable.Title>Nombres</DataTable.Title>
+        <DataTable.Title>Apellidos</DataTable.Title>
+        <DataTable.Title>Edad</DataTable.Title>
+      </DataTable.Header>
+      <DataTable.Row>
+        <DataTable.Cell>Radhika</DataTable.Cell>
+        <DataTable.Cell>Dosa</DataTable.Cell>
+        <DataTable.Cell>23</DataTable.Cell>
+      </DataTable.Row>
+  
+      <DataTable.Row>
+        <DataTable.Cell>Krishna</DataTable.Cell>
+        <DataTable.Cell>Uttapam</DataTable.Cell>
+        <DataTable.Cell>26</DataTable.Cell>
+      </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>Vanshika</DataTable.Cell>
+        <DataTable.Cell>Brownie</DataTable.Cell>
+        <DataTable.Cell>20</DataTable.Cell>
+      </DataTable.Row>
+      <DataTable.Row>
+        <DataTable.Cell>Teena</DataTable.Cell>
+        <DataTable.Cell>Pizza</DataTable.Cell>
+        <DataTable.Cell>24</DataTable.Cell>
+      </DataTable.Row>
+    </DataTable>
+  );
+};
+  
+export default TableExample;
+  
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+  },
+  tableHeader: {
+    backgroundColor: '#DCDCDC',
+  },
+});
